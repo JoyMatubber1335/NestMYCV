9d0c7d11825a77e42df3fd2f68fbbc69c99ed3fa
JoyMatubber1335 <joy.matubber@brainstation-23.com>
2024-07-11 11:03:23 +0600

    Connecton database sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
